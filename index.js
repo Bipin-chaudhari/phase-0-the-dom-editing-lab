da906c282acaa36f7ed000ee3f08f953e7aa09f3
+ <+>
2025-05-23 23:10:08 +1000

    dom lab

---
+++ index.js
@@ -0,0 +1,10 @@
+document.body.append(element);
+const ul = document.createElement("ul");
+
+for (let i = 0; i < 3; i++) {
+  const li = document.createElement("li");
+  li.textContent = (i + 1).toString();
+  ul.append(li);
+}
+
+element.append(ul);
